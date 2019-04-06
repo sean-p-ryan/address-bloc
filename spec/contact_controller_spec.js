819f5f7e87da55b85f044803aa3bb3ae411c1e2d
Sean Ryan <seanpryan89@gmail.com>
2019-04-05 20:01:31 -0400

    checkpoint completed

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -17,11 +17,12 @@ describe("ContactController", () => {
        describe("#addContact()", () => {
 
         it("should add a single contact into the book", (done) => {
-            this.book.addContact("Alice", "001-101-1010")
+            this.book.addContact("Alice", "001-101-1010", "alice123@hotmail.net")
             .then((contact) => {
 
               expect(contact.name).toBe("Alice");
               expect(contact.phone).toBe("001-101-1010");
+              expect(contact.email).toBe("alice123@hotmail.net");
               done();
             })
             .catch((err) => {
